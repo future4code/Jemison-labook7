c97788edbc4998909682ec2c4171f007376772f5
Leonardo Koga <leokoga@gmail.com>
2023-02-07 19:19:11 -0300

    teste dotenv no app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,30 @@
 import cors from "cors";
 import express from "express";
 import { AddressInfo } from "net";
+import knex from 'knex'
+import dotenv from 'dotenv'
 
 export const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+dotenv.config()
+
+export class BaseDatabase{
+   protected static connection = knex({
+      client: 'mysql',
+      connection: {
+         host: process.env.DB_HOST,
+         user: process.env.DB_USER,
+         password: process.env.DB_PASSWORD,
+         database: process.env.DB_SCHEMA,
+         port: 3306,
+         multipleStatements: true
+      }
+   })
+
+}
 
 const server = app.listen(process.env.PORT || 3003, () => {
    if (server) {
@@ -15,3 +34,4 @@ const server = app.listen(process.env.PORT || 3003, () => {
       console.log("Failure upon starting server");
    }
 })
+
