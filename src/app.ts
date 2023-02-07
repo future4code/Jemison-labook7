d3ad740629976c5340e83bf78beee7091bf38a5f
Leonardo Koga <leokoga@gmail.com>
2023-02-06 22:47:24 -0300

    AddressInfo

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import cors from "cors";
 import express from "express";
+import { AddressInfo } from "net";
 
 export const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.listen(process.env.PORT || 3003, () => {
-   console.log('Servidor rodando na porta 3003')
+const server = app.listen(process.env.PORT || 3003, () => {
+   if (server) {
+      const address = server.address() as AddressInfo
+      console.log(`Server is running in http://localhost:${address.port}`)
+   } else {
+      console.log("Failure upon starting server");
+   }
 })
